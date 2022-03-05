73759a6f5a48560a911bbb16d6135f0408bc7574
amelakadric <amela00108@gmail.com>
2022-03-05 16:54:49 +0100

    using ejs dinamically

---
+++ code/00-starting-project/app.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+
+const { urlencoded } = require("express");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/restaurants", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  res.redirect("/confirm");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.get("/index", function (req, res) {
+  res.render("index");
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.listen(3000);
